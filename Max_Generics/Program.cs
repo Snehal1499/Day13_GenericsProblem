d3d8c846cf4bfae37b2358c38e8a21d47753acb3
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-26 13:48:38 +0530

    Added Welcome Message

---
+++ Max_Generics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Max_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Maximum Number Problem");
+        }
+    }
+}
